abc039e031d1fa9ec01a78d430fecbcdaef5ff1b
Bobb Shields <bobbshields@gmail.com>
2013-03-27 16:33:26 -0400

    Minor heartbeat edits
    very minor.  mostly making messages hide if not in debug mode.

--- src/com/teamglokk/muni/listeners/MuniHeartbeat.java
+++ src/com/teamglokk/muni/listeners/MuniHeartbeat.java
@@ -6,7 +6,6 @@ package com.teamglokk.muni.listeners;
 
 import com.teamglokk.muni.Muni;
 import java.util.Date;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 /**
@@ -22,7 +21,7 @@ public class MuniHeartbeat extends BukkitRunnable {
     
     public void run() { 
         
-        plugin.getLogger().severe("[Muni Heartbeat] ");
+        if (plugin.isDebug()) {plugin.getLogger().info("[Muni Heartbeat] "); }
         
         long start = System.currentTimeMillis();
         int roundTo = 30; // 0 <= rT <= 60
@@ -30,7 +29,7 @@ public class MuniHeartbeat extends BukkitRunnable {
         Date roundedHour = new Date( Math.round ( (double) start / ( roundTo*2*60*1000 ) ) * (roundTo*2*60*1000) );
         Date exactDate = new Date (start);
         
-        plugin.getServer().broadcastMessage("Voting time!");
+        if (plugin.isDebug()) {plugin.getServer().broadcastMessage("[Muni] Voting coming soon!"); }
         // check for past due votes
             // announce reminders on the upcoming votes for the next 24 hours
             // decide their fate and announce to the appropriate town
